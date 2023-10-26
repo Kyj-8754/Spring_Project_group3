8c3057aefd14ec37e22779e74435b237e95f3052
sehwaniii <ha9948s@naver.com>
2023-10-26 17:02:56 +0900

    취합

--- group3/src/main/java/com/codingbox/group3/dto/LoginForm.java
+++ group3/src/main/java/com/codingbox/group3/dto/LoginForm.java
@@ -4,13 +4,11 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter @Setter
 public class LoginForm {
 
-  @NotEmpty(message = "회원 아이디는 필수 입니다.") // memberId가 비어있으면 안된다는 어노테이션
-  private String memberId;
-
-  @NotEmpty(message = "비밀번호는 필수 입니다.") // memberPw가 비어있으면 안된다는 어노테이션
-  private String memberPw;
+//	@NotEmpty(message = "회원 이름은 필수 입니다.") // name이 비어있으면 안된다는 어노테이션
+	private String memberId;
+	private String memberPw;
+	
 }
---
+++ group3/src/main/java/com/codingbox/group3/dto/StoreForm.java
@@ -0,0 +1,13 @@
+package com.codingbox.group3.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class StoreForm {
+	private String storeName;
+	private String storeAddr;
+	private String storePhone;
+	private String storeRoadAddr;
+	
+}
