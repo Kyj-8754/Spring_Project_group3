3edf1c7e9be5bc324152e82ef17fbbe33811acf9
sehwaniii <ha9948s@naver.com>
2023-10-26 16:03:07 +0900

    마이페이지 레포지토리 수정

--- group3/src/main/java/com/codingbox/group3/repository/MemberRepository.java
+++ group3/src/main/java/com/codingbox/group3/repository/MemberRepository.java
@@ -1,21 +1,31 @@
 package com.codingbox.group3.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.codingbox.group3.domain.Member;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
 
-	@Autowired
-	private final EntityManager em;
-	
-	public void save(Member member) {
-		em.persist(member);
-	}
+  @Autowired private final EntityManager em;
+
+  public void save(Member member) {
+    em.persist(member);
+  }
+
+  public Member findMemberByUserId(String userId) {
+    TypedQuery<Member> query =
+        em.createQuery("SELECT m FROM Member m WHERE m.userId = :userId", Member.class);
+    query.setParameter("userId", userId);
+    List<Member> resultList = query.getResultList();
+    return resultList.isEmpty() ? null : resultList.get(0);
+  }
 }
