059917b8612dc8ac7ab6dd70ca06dcbf0eac4186
Sehwaniii <142206581+Sehwaniii@users.noreply.github.com>
2023-10-27 16:56:47 +0900

    .

--- README.md
+++
@@ -1,3 +0,0 @@
-# Spring_Project_group3
-
-캐치테이블같은 식당 예약 사이트 구현
--- group3/src/main/java/com/codingbox/group3/session/SessionConst.java
+++
@@ -1,6 +0,0 @@
-package com.codingbox.group3.session;
-
-public class SessionConst {
-
-  public static final String LOGIN_MEMBER = "loginMember";
-}
